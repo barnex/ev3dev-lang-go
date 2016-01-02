16f693467082340b90b2b95602c27dd57594b7f0
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-01-02 21:18:08 +0000

    autogen: camelcase, letter receiver

--- autogen.go
+++ autogen.go
@@ -6,12 +6,18 @@ import (
 	"log"
 	"os"
 	"text/template"
+	"unicode"
 )
 
 func main() {
 	flag.Parse()
 	templFile := flag.Arg(0)
-	t, err := template.ParseFiles(templFile)
+	t := template.New(templFile)
+	t.Funcs(map[string]interface{}{
+		"camel": camelCase,
+		"recv": firstLetter,
+	})
+	t, err := t.ParseFiles(templFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,5 +34,30 @@ func main() {
 		log.Fatal(err)
 	}
 
-	t.Execute(os.Stdout, data)
+	err = t.Execute(os.Stdout, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func camelCase(x string) string {
+	cc := ""
+	needUp := true
+	for _, c := range x {
+		if c != '_' {
+			if needUp {
+				cc += string(unicode.ToUpper(c))
+				needUp = false
+			} else {
+				cc += string(c)
+			}
+		} else {
+			needUp = true
+		}
+	}
+	return cc
+}
+
+func firstLetter(x string) string {
+	return string(x[:1])
 }
