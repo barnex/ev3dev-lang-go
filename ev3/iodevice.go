b6a2b7cc7237b52ebfdb2f7be6aa6ebfd7880241
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-01-02 23:41:55 +0000

    cleanup

--- ev3/iodevice.go
+++ ev3/iodevice.go
@@ -8,11 +8,30 @@ func OpenIODevice(sysClass, devNameConvention, port string) (IODevice, error) {
 	panic("not implemented")
 }
 
-func (d IODevice) writeString(file string, x string)         { panic("not implemented") }
-func (d IODevice) writeStringSelector(file string, x string) { panic("not implemented") }
-func (d IODevice) writeInt(file string, x int)               { panic("not implemented") }
+func (d IODevice) writeString(file string, x string) {
+	panic("not implemented")
+}
 
-func (d IODevice) readInt(file string) int               { panic("not implemented") }
-func (d IODevice) readString(file string) string         { panic("not implemented") }
-func (d IODevice) readStringArray(file string) []string  { panic("not implemented") }
-func (d IODevice) readStringSelector(file string) string { panic("not implemented") }
+func (d IODevice) writeStringSelector(file string, x string) {
+	panic("not implemented")
+}
+
+func (d IODevice) writeInt(file string, x int) {
+	panic("not implemented")
+}
+
+func (d IODevice) readInt(file string) int {
+	panic("not implemented")
+}
+
+func (d IODevice) readString(file string) string {
+	panic("not implemented")
+}
+
+func (d IODevice) readStringArray(file string) []string {
+	panic("not implemented")
+}
+
+func (d IODevice) readStringSelector(file string) string {
+	panic("not implemented")
+}
