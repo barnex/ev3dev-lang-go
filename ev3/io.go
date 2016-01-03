daf9debcb60a23af2704160c5cc5124f60118e16
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-01-03 22:03:34 +0000

    implement IODevice read

--- ev3/io.go
+++ ev3/io.go
@@ -18,6 +18,8 @@ type IODevice struct {
 }
 
 func OpenIODevice(sysClass, devNameConvention, port string) (IODevice, error) {
+
+	// list all files under /sys/class/<sysClass>
 	sysDir := path.Join(SysFS, sysClass)
 	dir, err := os.Open(sysDir)
 	if err != nil {
@@ -28,16 +30,17 @@ func OpenIODevice(sysClass, devNameConvention, port string) (IODevice, error) {
 	if err != nil {
 		return IODevice{}, fmt.Errorf("open %v:%v: %v", sysClass, port, err)
 	}
-
+	sort.Strings(ls)
 	if len(ls) == 0 {
 		return IODevice{}, fmt.Errorf("open %v:%v: %v", sysClass, port, "no devices present")
 	}
 
-	sort.Strings(ls)
+	// no port specified: return first matching
 	if port == "" {
 		return IODevice{path: path.Join(sysDir, ls[0])}, nil
 	}
 
+	// search port_name files for matching port
 	for _, f := range ls {
 		if readFile(path.Join(sysDir, f, "port_name")) == port {
 			return IODevice{path: path.Join(sysDir, f)}, nil
@@ -55,16 +58,32 @@ func readFile(f string) string {
 	return strings.TrimSpace(string(bytes))
 }
 
+func(d IODevice)write(file string, x interface{}){
+	f, err := os.OpenFile(path.Join(d.path, file), os.O_WRONLY, 0666)
+	if err != nil{panic (err)}
+	defer f.Close()
+	fmt.Println(f, x)
+}
+
+func(d IODevice)read(file string) []byte{
+	f, err := os.OpenFile(path.Join(d.path, file), os.O_RDONLY, 0666)
+	if err != nil{panic (err)}
+	defer f.Close()
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil{panic(err)}
+	return bytes
+}
+
 func (d IODevice) writeString(file string, x string) {
-	panic("not implemented")
+	d.write(file, x)
 }
 
 func (d IODevice) writeStringSelector(file string, x string) {
-	panic("not implemented")
+	d.write(file, x)
 }
 
 func (d IODevice) writeInt(file string, x int) {
-	panic("not implemented")
+	d.write(file, x)
 }
 
 func (d IODevice) readInt(file string) int {
@@ -72,13 +91,13 @@ func (d IODevice) readInt(file string) int {
 }
 
 func (d IODevice) readString(file string) string {
-	panic("not implemented")
+	return string(d.read(file))
 }
 
 func (d IODevice) readStringArray(file string) []string {
-	panic("not implemented")
+	return strings.Split(d.readString(file), " ")
 }
 
 func (d IODevice) readStringSelector(file string) string {
-	panic("not implemented")
+	return d.readString(file)
 }
